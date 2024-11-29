8b5a80912d78ac920460677759f336ef803d1b93
Brayden Bourgeois <you@example.com>
2024-11-29 17:55:02 -0500

    created function to display student info

--- script.js
+++ script.js
@@ -5,4 +5,13 @@
     age: 24,
     enrolled: true,
     courses: ["Science", " Cooking", " Math", " History"],
+
+    //function to display student information
+    displayInfo: function() {
+        console.log("Student name: " + this.name);
+        console.log("Student age: " + this.age);
+        console.log("Is student enrolled?: " + this.enrolled);
+        console.log("Current courses: " + this.courses);
+    }
+
 }
